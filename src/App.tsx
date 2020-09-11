9f37d085e941f95883ad92b08d257bf8748042d0
Dearblackswan <drl8@me.com>
2020-09-11 15:20:41 +0200

    clean the repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,145 @@
 import React from 'react';
+import Form from "@rjsf/fluent-ui";
+import { JSONSchema7 } from "json-schema";
 
-import logo from './logo.svg';
-import './App.css';
+
+const uiSchema = {
+  "simple": {
+    "credit_card": {
+      "ui:help": "If you enter anything here then billing_address will be dynamically added to the form."
+    }
+  },
+  "conditional": {
+    "Do you want to get rid of any?": {
+      "ui:widget": "radio"
+    }
+  },
+  "arrayOfConditionals": {
+    "items": {
+      "Do you want to get rid of any?": {
+        "ui:widget": "radio"
+      }
+    }
+  }
+}
 
 
+const schema: JSONSchema7 =  {
+"title": "Schema ",
+"description": "",
+"type": "object",
+"properties": {
+  "simple": {
+    "title": "Simple",
+    "type": "object",
+    "properties": {
+      "name": {
+        "type": "string"
+      },
+      "credit_card": {
+        "type": "number"
+      }
+    },
+    "required": [
+      "name"
+    ],
+    "dependencies": {
+      "credit_card": {
+        "properties": {
+          "billing_address": {
+            "type": "string"
+          }
+        },
+        "required": [
+          "billing_address"
+        ]
+      }
+    }
+  },
+  "conditional": {
+    "title": "Conditional",
+    "$ref": "#/definitions/person"
+  },
+  "arrayOfConditionals": {
+    "title": "Array of conditionals",
+    "type": "array",
+    "items": {
+      "$ref": "#/definitions/person"
+    }
+  }
+},
+"definitions": {
+  "person": {
+    "title": "Person",
+    "type": "object",
+    "properties": {
+      "Do you have any pets?": {
+        "type": "string",
+        "enum": [
+          "No",
+          "Yes: One",
+          "Yes: More than one"
+        ],
+        "default": "No"
+      }
+    },
+    "required": [
+      "Do you have any pets?"
+    ],
+    "dependencies": {
+      "Do you have any pets?": {
+        "oneOf": [
+          {
+            "properties": {
+              "Do you have any pets?": {
+                "enum": [
+                  "No"
+                ]
+              }
+            }
+          },
+          {
+            "properties": {
+              "Do you have any pets?": {
+                "enum": [
+                  "Yes: One"
+                ]
+              },
+              "How old is your pet?": {
+                "type": "number"
+              }
+            },
+            "required": [
+              "How old is your pet?"
+            ]
+          },
+          {
+            "properties": {
+              "Do you have any pets?": {
+                "enum": [
+                  "Yes: More than one"
+                ]
+              },
+              "Do you want to get rid of any?": {
+                "type": "boolean"
+              }
+            },
+            "required": [
+              "Do you want to get rid of any?"
+            ]
+          }
+        ]
+      }
+    }
+  }
+}
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <Form schema={schema} uiSchema={uiSchema} />
+
   );
 }
 
